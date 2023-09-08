2f1ab99d9d4df16e91e740e462ba07f2d05cdef5
GAON PARK <do.judo1224@gmail.com>
2023-09-08 15:31:07 +0900

    최소 스패닝 트리

---
+++ 0x1B 최소 신장 트리/1197.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Edge {
+	long long cost;
+	int a, b;
+
+	bool operator<(Edge e) const {
+		return cost < e.cost;
+	}
+};
+
+int v, e;
+vector<Edge> vec;
+int parent[10010]; // union find 
+
+int find_parent(int a) {
+	if (a == parent[a]) return a;
+	return parent[a] = find_parent(parent[a]);
+}
+
+void union_parent(int a, int b) {
+	a = find_parent(a);
+	b = find_parent(b);
+	if (a < b) {
+		parent[b] = a;
+	}
+	else if (b < a) {
+		parent[a] = b;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> v >> e;
+	for (int i = 1; i <= v; i++) { // 정점은 1번~V번
+		parent[i] = i; // 자기 자신을 부모로 초기화
+	}
+
+	for (int i = 0; i < e; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		vec.push_back({ c, a, b });
+	}
+	sort(vec.begin(), vec.end()); // 가중치 오름차순
+
+	long long ans = 0;
+	for (auto& cur : vec) {
+		if (find_parent(cur.a) == find_parent(cur.b)) continue;
+		union_parent(cur.a, cur.b);
+		ans += cur.cost;
+	}
+
+	cout << ans;
+}
